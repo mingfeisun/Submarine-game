3e8799311e6f335af3edde02f871e273dabe06c9
Mingfei Sun <mingfei.sun@ust.hk>
2017-03-25 22:43:39 +0800

    finish the setup stage, place objects and check the accessability

---
+++ game.js
@@ -0,0 +1,218 @@
+var cButton = document.getElementById("button");
+var cField = document.getElementById("field");
+var nRounds = document.getElementById("rounds");
+var nScoreUser = document.getElementById("score_user");
+var nScoreComputer = document.getElementById("score_computer");
+var nRemainingFuel = document.getElementById("remaining_fuel");
+
+var CurrentKillerPos = [];
+var CurrentUserPos = [];
+var CurrentFuel = 0;
+var CurrentUserScore = 0;
+var CurrentComputerScore = 0;
+var CurrentRound = 0;
+
+var SelectedLeft = 0;
+var SelectedTop = 0;
+
+var OBSTACLE = 1e3;
+
+var HINT_SETUP = "type 1 - 9 to place a fuel cell," +
+                 "o to place an obstacle, " +
+                 "u to place your submarine, " +
+                 "k to place a robotic killer, " +
+                 "ESC to cancel.";
+
+var ALERT_OCCUPIED = "This place has already been set!";
+var ALERT_ALREADY_SET = "Your submarine has already been set!";
+
+var GridMap = new Array(10);
+for (var i = 0; i < 10; i++){
+    GridMap[i] = new Array(10);
+    GridMap[i].fill(0);
+}
+
+window.document.onload = init();
+
+function init() {
+    cButton.innerHTML = "START";
+    nRounds.innerHTML = "0";
+    nScoreUser.innerHTML = "0";
+    nScoreComputer.innerHTML = "0";
+    nRemainingFuel.innerHTML = "0";
+    cButton.addEventListener("click", buttonClick);
+}
+
+function buttonClick() {
+    if (CurrentKillerPos.length == 0 || CurrentUserPos.length == 0) {
+        document.getElementById("start_hint").innerHTML = "Setup first here by clicking!";
+        cField.setAttribute("class", "cursorPointer");
+        cField.addEventListener("click", fieldClickClear);
+    } else {
+        CurrentRound = 1;
+        CurrentFuel = 10;
+        return;
+    }
+}
+
+function fieldClickClear() {
+    document.getElementById("start_hint").innerHTML = ""; // clear all texts
+    cField.removeEventListener("click", fieldClickClear);
+    cField.addEventListener("click", fieldClick);
+    cField.addEventListener("mousemove", fieldMouseOver);
+    return;
+}
+
+function fieldClick() {
+    var selectedDiv = document.getElementById("selected");
+
+    SelectedLeft = parseInt(selectedDiv.style.left.split("px")[0]);
+    SelectedTop = parseInt(selectedDiv.style.top.split("px")[0]);
+
+    // check whether it's already been set
+    if (GridMap[SelectedLeft/64][SelectedTop/64] != 0){
+        createHintDiv(ALERT_OCCUPIED);
+        setTimeout(function () { createHintDiv(""); }, 2000);
+        return;
+    }
+
+    cField.removeEventListener("mousemove", fieldMouseOver);
+    cField.removeEventListener("click", fieldClick);
+
+    selectedDiv.style.borderColor = "red";
+
+    // create a hint window
+
+    createHintDiv(HINT_SETUP);
+    window.addEventListener("keydown", fieldConfig);
+}
+
+function createHintDiv(msg) {
+    var child = document.getElementById("hint");
+    if (child != null ){
+        cField.removeChild(child);
+    }
+
+    var hintDiv = document.createElement("DIV");
+    var hintText = document.createTextNode(msg);
+    hintDiv.appendChild(hintText);
+    hintDiv.setAttribute("class", "hintdiv");
+    hintDiv.setAttribute("id", "hint");
+
+    var offset = SelectedLeft;
+    if (offset + 300 >= 640){
+        offset = 340;
+    }
+    hintDiv.style.left = offset + "px";
+
+    offset = SelectedTop;
+    if (offset + 64 >= 640){
+        offset = offset - 64;
+    }
+    else{
+        offset = offset + 64;
+    }
+    hintDiv.style.top = offset + "px";
+    cField.appendChild(hintDiv);
+}
+
+function fieldConfig(event) {
+    var hasProcessed = false;
+
+    // place an object
+    if (event.key >= "1" && event.key <= "9"){
+        GridMap[SelectedLeft/64][SelectedTop/64] = event.key;
+        placeObject("fuel-"+event.key);
+        hasProcessed = true;
+    }
+    else{
+        switch(event.key){
+            case "Escape":
+                hasProcessed = true;
+                break;
+
+            case "o":
+                GridMap[SelectedLeft/64][SelectedTop/64] = OBSTACLE;
+                placeObject("obstacle");
+                hasProcessed = true;
+                break;
+
+            case "u":
+                if (CurrentUserPos.length == 0){
+                    GridMap[SelectedLeft/64][SelectedTop/64] = -1;
+                    CurrentUserPos.push(SelectedLeft/64);
+                    CurrentUserPos.push(SelectedTop/64);
+                    placeObject("submarine");
+                    hasProcessed = true;
+                }
+                else{
+                    createHintDiv(ALERT_ALREADY_SET);
+                    setTimeout(function () { createHintDiv(HINT_SETUP); }, 2000);
+                }
+                break;
+
+            case "k":
+                GridMap[SelectedLeft/64][SelectedTop/64] = -1;
+                CurrentKillerPos.push(SelectedLeft/64);
+                CurrentKillerPos.push(SelectedTop/64);
+                placeObject("killer");
+                hasProcessed = true;
+                break;
+
+            default:
+                alert("Only key 1-9, o, u, k allowed!");
+                break;
+        }
+    }
+
+    // Has been processed
+    if (hasProcessed){
+        // remove hint & selected div
+        var child = document.getElementById("hint");
+        if (child != null ){
+            cField.removeChild(child);
+        }
+        child = document.getElementById("selected");
+        if (child != null ){
+            cField.removeChild(child);
+        }
+
+        // remove listener
+        window.removeEventListener("keydown", fieldConfig);
+
+        // add listeners
+        cField.addEventListener("mousemove", fieldMouseOver);
+        cField.addEventListener("click", fieldClick);
+    }
+}
+
+function placeObject(obj) {
+    var imgElem = document.createElement("IMG");
+    imgElem.setAttribute("src", "images/"+obj+".png");
+    var objectDiv = document.createElement("DIV");
+    objectDiv.setAttribute("class", "objectdiv");
+    objectDiv.style.left = SelectedLeft + "px";
+    objectDiv.style.top = SelectedTop + "px";
+    objectDiv.appendChild(imgElem);
+    cField.appendChild(objectDiv);
+}
+
+function fieldMouseOver(event) {
+    var child = document.getElementById("selected");
+    if (child != null){
+        cField.removeChild(child);
+    }
+
+    // create a shadowing div
+    var rect = cField.getBoundingClientRect();
+    var xOffset = Math.floor((event.pageX - rect.left)/64) * 64;
+    var yOffset = Math.floor((event.pageY - rect.top)/64) * 64;
+    var borderDiv = document.createElement("DIV");
+    borderDiv.setAttribute("class", "borderdiv");
+    borderDiv.setAttribute("id", "selected");
+    borderDiv.style.left = xOffset + "px";
+    borderDiv.style.top = yOffset + "px";
+
+    cField.appendChild(borderDiv);
+}
+
