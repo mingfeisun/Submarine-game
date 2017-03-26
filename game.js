fd89c108706d4239e76c1ca49c207dd369771142
Mingfei Sun <mingfei.sun@ust.hk>
2017-03-26 17:42:29 +0800

    complete version with user movement bugs

--- game.js
+++ game.js
@@ -22,6 +22,7 @@ var CurrentFuel = 0;
 var CurrentUserScore = 0;
 var CurrentComputerScore = 0;
 var CurrentRound = 0;
+var FuelCount = 0;
 
 
 
@@ -67,11 +68,15 @@ var HINT_MOVE_TO_OBSTACLE_T = 1504;
 var HINT_MOVE_TO_KILL = "Kill position! Move fails...";
 var HINT_MOVE_TO_KILL_T = 1505;
 
-var HINT_FUEL_GET = "Fuels + ";
+var HINT_FUEL_GET1 = "Fuels + ";
+var HINT_FUEL_GET2 = ", Scores + ";
 var HINT_FUEL_GET_T = 1001;
 
 var HINT_FUEL_LOSE = "Fuels - 1";
 var HINT_FUEL_LOSE_T = 501;
+
+var HINT_KILLER_SCORE_GET = "Killer Scores +";
+var HINT_KILLER_SCORE_GET_T = 401;
 
 
 
@@ -88,11 +93,8 @@ var HINT_FUEL_LOSE_T = 501;
 var VAL_OBSTACLE = 1e3;
 
 var GridMap = new Array(10);
-for (var i = 0; i < 10; i++){
-    GridMap[i] = new Array(10);
-    GridMap[i].fill(0);
-}
 
+var IsRunning = -2;
 window.document.onload = init();
 
 
@@ -113,18 +115,54 @@ function init() {
 
 // button click callback
 function buttonClick() {
-    if (CurrentKillerPos.length == 0 || CurrentUserPos.length == 0) {
+    if (IsRunning < -1) {
         showAlert("Setup first here by clicking!", 2000);
         setupInit();
     }
-    else {
+    else if(IsRunning < 0) {
+        clearTimeouts();
         showAlert("Game Starts! Please move your submarine.", 1500);
         playInit();
     }
+    else if(IsRunning < 1){
+        finalOutput();
+    }
+    else {
+        clearSettings();
+        showAlert("Setup first here by clicking!", 2000);
+        setupInit();
+    }
+}
+
+function clearTimeouts() {
+    // remove all timeouts
+    var highestTimeoutId = setTimeout("null");
+    for (var i = 0 ; i < highestTimeoutId ; i++) {
+        clearTimeout(i);
+    }
+}
+
+function clearSettings() {
+    while(cField.firstChild){
+        cField.removeChild(cField.firstChild);
+    }
+    CurrentKillerPos = []; // 0-9, 0-9
+    CurrentUserPos = []; // 0-9, 0-9
+    CurrentFuel = 0;
+    CurrentUserScore = 0;
+    CurrentComputerScore = 0;
+    CurrentRound = 0;
+    FuelCount = 0;
+
+    SelectedLeft = 0;
+    SelectedTop = 0;
 }
 
 // play init
 function playInit() {
+    // game running flag
+    IsRunning = 0;
+
     // update button
     cButton.innerHTML = "STOP";
 
@@ -147,6 +185,7 @@ function playInit() {
     // reset SelectedLeft & SelectedTop
     SelectedLeft = CurrentUserPos[0]*64;
     SelectedTop = CurrentUserPos[1]*64;
+
 
     // add listeners for keydown
     window.addEventListener("keydown", controlSubmarine);
@@ -185,15 +224,15 @@ function updateUserPosition(xShiftVal, yShiftVal) {
 
     if (estimatedPosX < 0 || estimatedPosX >9 || estimatedPosY < 0 || estimatedPosY > 9){
         showHint(HINT_MOVE_OUT_OF_RANGE, HINT_MOVE_OUT_OF_RANGE_T);
-        window.addEventListener("keydown", controlSubmarine);
+        // window.addEventListener("keydown", controlSubmarine);
     }
     else if (GridMap[estimatedPosX][estimatedPosY] == VAL_OBSTACLE){
         showHint(HINT_MOVE_TO_OBSTACLE, HINT_MOVE_TO_OBSTACLE_T);
-        window.addEventListener("keydown", controlSubmarine);
+        // window.addEventListener("keydown", controlSubmarine);
     }
     else if (GridMap[estimatedPosX][estimatedPosY] == -1){
         showHint(HINT_MOVE_TO_KILL, HINT_MOVE_TO_KILL_T);
-        window.addEventListener("keydown", controlSubmarine);
+        // window.addEventListener("keydown", controlSubmarine);
     }
     else{
         moveUser(xShiftVal, yShiftVal);
@@ -242,8 +281,11 @@ function moveUser(xVal, yVal) {
             setTimeout(movit, 20);
         }
         else{
-            CurrentUserPos[0] += xVal;
-            CurrentUserPos[1] += yVal;
+            GridMap[CurrentUserPos[0]][CurrentUserPos[1]] = 0;
+
+            CurrentUserPos[0] = targetX;
+            CurrentUserPos[1] = targetY;
+
             SelectedLeft = CurrentUserPos[0]*64;
             SelectedTop = CurrentUserPos[1]*64;
 
@@ -253,16 +295,26 @@ function moveUser(xVal, yVal) {
 
                 // update the gridmap
                 var addFuel = GridMap[targetX][targetY];
-                GridMap[targetX][targetY] = 0;
 
-                showHint(HINT_FUEL_GET + addFuel, HINT_FUEL_GET_T);
+                showHint(HINT_FUEL_GET1 + addFuel + HINT_FUEL_GET2 + addFuel, HINT_FUEL_GET_T);
                 CurrentFuel += parseInt(addFuel);
+                CurrentUserScore += parseInt(addFuel);
+
+                FuelCount -= 1;
+                if (FuelCount == 0){
+                    updateStatus();
+                    finalOutput();
+                    return;
+                }
             }
+            GridMap[targetX][targetY] = -2;
+
             updateStatus();
             if (CurrentFuel == 0){
                 finalOutput();
                 return;
             }
+
             updateKillerPosition();
             window.addEventListener("keydown", controlSubmarine);
         }
@@ -271,8 +323,15 @@ function moveUser(xVal, yVal) {
 
 
 function finalOutput() {
+    // game over flag
+    IsRunning = 1;
+
+    window.removeEventListener("keydown", controlSubmarine);
+
+    cButton.innerHTML = "START";
+
     var finalRes = "";
-    if (CurrentUserScore > CurrentKillerPos){
+    if (CurrentUserScore > CurrentComputerScore){
         finalRes = "WIN ^o^ !";
     }
     else{
@@ -283,12 +342,162 @@ function finalOutput() {
 
 
 function updateKillerPosition() {
-    moveKiller();
+    for (var i = 0; i < CurrentKillerPos.length/2; i++){
+        var killerX = CurrentKillerPos[2*i];
+        var killerY = CurrentKillerPos[2*i+1];
+
+        var offsetX = Infinity;
+        var offsetY = Infinity;
+
+        // find any fuels or user nearby, select the maximum fuel or user
+        var maxFuel = 0;
+        var emptyCount = 0;
+        var lastX = Infinity;
+        var lastY = Infinity;
+        for (var j = -1; j <= 1; j++){
+            if (killerX + j < 0 || killerX + j > 9){
+                continue;
+            }
+            for (var k = -1; k <= 1; k++){
+                if (killerY + k < 0 || killerY + k > 9){
+                    continue;
+                }
+                if (GridMap[killerX + j][killerY + k] >= 0 && GridMap[killerX + j][killerY + k] < VAL_OBSTACLE){
+                    emptyCount += 1;
+                    lastX = j; lastY = k;
+                }
+
+                if (GridMap[killerX + j][killerY + k] == -2){
+                    offsetX = j;
+                    offsetY = k;
+                    maxFuel = -1;
+                    break;
+                }
+                if (GridMap[killerX + j][killerY + k] > maxFuel && GridMap[killerX + j][killerY + k] < VAL_OBSTACLE){
+                    maxFuel = GridMap[killerX + j][killerY + k];
+                    offsetX = j;
+                    offsetY = k;
+                }
+            }
+            if (maxFuel == -1){
+                break;
+            }
+        }
+
+        // no empty spaces for this killer to move
+        if (emptyCount == 0){
+            continue;
+        }
+
+        // no fuels found, uniformly pick an empty position
+        if (offsetX == Infinity || offsetY == Infinity){
+            for (var j = -1; j <= 1; j++){
+                if (killerX + j < 0 || killerX + j > 9){
+                    continue;
+                }
+                for (var k = -1; k <= 1; k++){
+                    if (killerY + k < 0 || killerY + k > 9){
+                        continue;
+                    }
+                    if (GridMap[killerX + j][killerY + k] >= 0 && GridMap[killerX + j][killerY + k] < VAL_OBSTACLE && Math.random() >= (1- 1/emptyCount)){
+                        offsetX = j;
+                        offsetY = k;
+                        break;
+                    }
+                }
+                if (offsetX != -10 && offsetY != -10){
+                    break;
+                }
+            }
+        }
+
+        // randomly selection fails
+        if (offsetX == Infinity || offsetY == Infinity){
+            offsetX = lastX; offsetY = lastY;
+        }
+
+        moveKiller(i, killerX, killerY, offsetX, offsetY);
+    }
 }
 
 // move killer
-function moveKiller() {
-    
+function moveKiller(index, startX, startY, offsetX, offsetY) {
+    var killerDiv = document.getElementById("killer"+startX+startY);
+
+    var stepsX = 0;
+    var stepsY = 0;
+    if (offsetX != 0 ){
+        stepsX = offsetX * 1; // set moving steps to 4;
+    }
+    if (offsetY != 0 ){
+        stepsY = offsetY * 1; // set moving steps to 4;
+    }
+
+    var currentX = parseInt(killerDiv.style.left.split("px")[0]);
+    var currentY = parseInt(killerDiv.style.top.split("px")[0]);
+    var targetX = (startX + offsetX);
+    var targetY = (startY + offsetY);
+
+    var fuelDiv = null;
+    if (GridMap[targetX][targetY] > 0 && GridMap[targetX][targetY] < 10){
+        fuelDiv = document.getElementById("fuel"+targetX+targetY);
+        fuelDiv.style.opacity = 1;
+    }
+
+    movit();
+
+    function movit() {
+        var offOpa = 0.05;
+        if (currentX != targetX*64 || currentY != targetY*64){
+            currentX += stepsX;
+            currentY += stepsY;
+            killerDiv.style.left = currentX + "px";
+            killerDiv.style.top = currentY + "px";
+            if (fuelDiv != null && parseFloat(fuelDiv.style.opacity) > 0){
+                fuelDiv.style.opacity = parseFloat(fuelDiv.style.opacity) - offOpa;
+            }
+            setTimeout(movit, 20);
+        }
+        else{
+            CurrentKillerPos[index*2] = targetX;
+            CurrentKillerPos[index*2 + 1] = targetY;
+            killerDiv.setAttribute("id", "killer"+targetX+targetY);
+
+
+            CurrentRound += 1;
+
+            if (targetX == CurrentUserPos[0] && targetY == CurrentUserPos[1]){
+                CurrentComputerScore += 100;
+                updateStatus();
+                finalOutput();
+                return;
+            }
+
+            if (fuelDiv != null){
+                // remove the div
+                cField.removeChild(fuelDiv);
+
+                // update the gridmap
+                var addFuel = GridMap[targetX][targetY];
+
+                showHint(HINT_KILLER_SCORE_GET + addFuel, HINT_KILLER_SCORE_GET_T + index, targetX*64, targetY*64);
+                CurrentComputerScore += parseInt(addFuel);
+
+                FuelCount -= 1;
+                if (FuelCount == 0){
+                    updateStatus();
+                    finalOutput();
+                    return;
+                }
+            }
+
+            GridMap[startX][startY] = 0;
+            GridMap[targetX][targetY] = -1;
+
+            updateStatus();
+        }
+    }
+
 }
 
 // update playing info
@@ -301,6 +510,14 @@ function updateStatus() {
 
 // setup init
 function setupInit() {
+    // game setup flag
+    IsRunning = -1;
+
+    for (var i = 0; i < 10; i++){
+        GridMap[i] = new Array(10);
+        GridMap[i].fill(0);
+    }
+
     cField.setAttribute("class", "cursorPointer");
     cField.addEventListener("click", fieldClick);
     cField.addEventListener("mousemove", fieldMouseOver);
@@ -345,7 +562,7 @@ function showAlert(msg, duration) {
 }
 
 // show hint info over the field
-function showHint(msg, duration){
+function showHint(msg, duration, xPixel, yPixel){
     // check existing div and mark it
     // var child = document.getElementById("hint");
     // var affixID = "";
@@ -353,19 +570,24 @@ function showHint(msg, duration){
     //     affixID = duration; // for overlay effect
     // }
 
+    if (xPixel === undefined || yPixel === undefined){
+        xPixel = SelectedLeft;
+        yPixel = SelectedTop;
+    }
+
     var hintDiv = document.createElement("DIV");
     var hintText = document.createTextNode(msg);
     hintDiv.appendChild(hintText);
     hintDiv.setAttribute("class", "hintdiv");
     hintDiv.setAttribute("id", "hint"+duration);
 
-    var offset = SelectedLeft;
+    var offset = xPixel;
     if (offset + 300 >= 640){
         offset = 340;
     }
     hintDiv.style.left = offset + "px";
 
-    offset = SelectedTop;
+    offset = yPixel;
     if (offset + 64 >= 640){
         offset = offset - 64;
     }
@@ -388,6 +610,7 @@ function fieldConfig(event) {
     // place an object
     if (event.key >= "1" && event.key <= "9"){
         GridMap[SelectedLeft/64][SelectedTop/64] = event.key;
+        FuelCount += 1;
         placeObject("fuel-"+event.key);
         hasProcessed = true;
     }
